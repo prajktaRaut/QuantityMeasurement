e180763f418067da54aa71f966d1b5d982a1e3fa
mahendrakundare <mr.kundare@gmail.com>
2019-12-17 13:53:25 +0530

    Added Code and Test for Checking Equality of Inch Objects

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityTest.java
@@ -11,4 +11,26 @@ public class QuantityTest {
         Feet feet2= new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
+    }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEquals() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertFalse(false);
+    }
 }
